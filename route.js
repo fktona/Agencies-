9eb4f3c83b5897cb586377fe340bf08ba4106f80
fktona <113286902+fktona@users.noreply.github.com>
2023-12-01 14:05:03 +0100

    Update route.js

--- route.js
+++ route.js
@@ -3,9 +3,10 @@ const router = express.Router();
 
 const {get_agencies_by_location} = require('./Controller/filter')
 const {get_search} = require('./Controller/search')
-const {create_agencies , upload ,get_agency} = require('./Controller/agencies')
+const {create_agencies , upload ,get_agency, getAgencyById} = require('./Controller/agencies')
 
 router.post('/create' , upload.single('logo'), create_agencies)
+router.get('/agency/:id', getAgencyById)
 router.get('/search' , get_search)
 router.get('/agency' ,  get_agency)
 router.get('/search/location' ,  get_agencies_by_location)
