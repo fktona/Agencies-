c50ab898226bc3305da4103da600894163cd5ff6
fktona <113286902+fktona@users.noreply.github.com>
2024-01-13 15:14:11 +0100

    Update route.js

--- route.js
+++ route.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router();
 
-const {get_agencies_by_location} = require('./Controller/filter')
+const {get_agencies_by_location , get_location} = require('./Controller/filter')
 const {get_search} = require('./Controller/search')
 const {create_agencies , upload ,get_agency, getAgencyById ,approveAgency,getApprovedAgencies} = require('./Controller/agencies')
 
@@ -12,5 +12,6 @@ router.patch('/agency/approve/:agencyId', approveAgency)
 router.get('/search' , get_search)
 router.get('/agency' ,  get_agency)
 router.get('/search/location' ,  get_agencies_by_location )
+router.get('/get_location' ,  get_location)
 
 module.exports = router 
